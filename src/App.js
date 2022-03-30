3cc15a2e801c71af72ed80eddaa07280f9ff1c9a
sahilraj-developer <sahil27raj@gmail.com>
2022-03-30 14:17:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { data } from "./birthday-data";
+import "./App.css";
 
 function App() {
+  const [people, setPeople] = useState(data);
+
+
+  const removePerson = (id) => {
+    let newPerson = people.filter((person) => person.id !== id);
+    setPeople(newPerson);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 style={{color: " rgb(23, 180, 88)", textAlign: "center",fontSize: "36px", background: "#fff", }} >
+        {people.length} Birthday Today
+      </h3>
+      <button className="btn" onClick={() => setPeople(data)}>
+        Restore Data
+      </button>
+      {people.map((person) => {
+      
+        return (
+          <div className="container" key={person.id}>
+            <div className="avatar">
+              <div>
+                <img src={person.image} alt="" />
+              </div>
+
+              <div>
+                <h3>{person.name}</h3>
+              </div>
+            </div>
+            <div className="button">
+              <button className="btn" onClick={() => removePerson(person.id)}>
+                Dismiss
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/birthday-data.js
@@ -0,0 +1,7 @@
+export const data = [
+  { id: 1, name: "sahil", image: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" },
+  { id: 2, name: "ajit", image: "https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1140180560?k=20&m=1140180560&s=612x612&w=0&h=X_400OQDFQGqccORnKt2PHYvTZ3dBLeEnCH_hRiUQrY=" },
+  { id: 3, name: "vishal", image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" },
+  { id: 4, name: "raja", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFU7U2h0umyF0P6E_yhTX45sGgPEQAbGaJ4g&usqp=CAU" },
+  { id: 5, name: "sourav", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbrvOZf5zaHg_9a8upGltfVtObFu_0QH1rcw&usqp=CAU" },
+];
